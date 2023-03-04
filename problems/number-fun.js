8dd120f40331c44f20f6a5f0ff168940eab9f4e2
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-03-04 22:03:28 +0545

    Completed

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,5 +1,8 @@
 let returnsThree=()=>3;
 let reciprocal=n=>{
+  if(n<1 || n>1000000){
+    throw new TypeError("Number is less than 1 or greater than 1000000");
+  }
   if(n===0){
     return 0;
   }
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -7,9 +7,15 @@ describe("ReturnsThree Function",function(){
 });
 describe("Reciprocal Function",function(){
     it("should return reciprocal",function(){
-        expect(reciprocal(0)).to.equal(0);
+        
         expect(reciprocal(1)).to.equal(1);
         expect(reciprocal(4)).to.equal(0.25);
-        expect(reciprocal(-4)).to.equal(-0.25);
+        
+
+    })
+    it("should thorw Error",function(){
+        expect(()=>reciprocal(0)).to.throw(Error);
+        expect(()=>reciprocal(-4)).to.throw(Error);
+        expect(()=>reciprocal(1000001)).to.throw(Error);
     })
 });
