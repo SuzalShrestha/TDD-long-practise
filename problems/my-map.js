3b5ebffcaefe9128e1c926a66de5cb28779c538b
sujal_shrestha <sujal.scxi121@amv.edu.np>
2023-03-05 08:53:52 +0545

    Completed

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,10 @@
 function myMap(inputArray, callback) {
   // Your code here
+  let newArr=[];
+  for(let item of inputArray){
+    newArr.push(callback(item));
+  }
+  return newArr;
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,29 @@
-// Your code here
+const chai=require('chai');
+const {expect}=require('chai');
+const myMap=require("../problems/my-map");
+const spies = require('chai-spies');
+chai.use(spies);
+
+describe("My map",function(){
+    let arr;
+    let call;
+    beforeEach(function(){
+       arr=[1,2,3];
+       call=function(num){return num*2;};
+    })
+    it("should not mutate the original array",function(){
+        let array=[1,2,3];
+        myMap(arr,call);
+        expect(arr).to.eql(array);
+    })
+    it("should not call built-in Array.map method",function(){
+        let spy=chai.spy.on(arr,'map',myMap(arr,call));
+        expect(spy).to.have.been.not.called();
+    })
+    it("should call passed in callback for each element of array",function(){
+        let spy=chai.spy();
+        myMap(arr,spy);
+        expect(spy).to.have.been.called.exactly(3);
+    });
+    
+});
